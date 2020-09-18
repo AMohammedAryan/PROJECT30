0b58f74d6713bbd85b1b0181bda2925d7b62fc1c
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-09-18 19:18:27 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,38 @@
+class Block{
+  constructor(x, y, width, height){
+    var options = {
+      'restitution':0.8,
+      'friction':1.0,
+      'density':1.0,
+      isStatic:false
+     }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+
+    this.Visibility = 255;
+  }
+
+  display(){
+    var angle = this.body.angle
+
+    rectMode(CENTER);
+    fill("lightBlue");
+
+    if(this.body.speed < 3){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rect(0, 0, this.width, this.height);
+        pop();
+    }
+    else{
+      this.Visibility = this.Visibility - 10;
+    }
+
+    if(this.Visibility < 1){
+      World.remove(world, this.body);
+    }
+  }
+}
